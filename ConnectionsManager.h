a23bacb9cef88ab7d0e6162d9ebcf70d0f40a6ba
taciturn2021 <darkraiden112@gmail.com>
2024-12-12 13:48:01 +0500

    Make a proper cmake project.

---
+++ ConnectionsManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Muhammad Abdullah on 12/12/2024.
+//
+
+#ifndef CONNECTIONSMANAGER_H
+#define CONNECTIONSMANAGER_H
+
+
+
+class ConnectionsManager {
+
+};
+
+
+
+#endif //CONNECTIONSMANAGER_H
--- User.cpp
+++ User.cpp
@@ -63,9 +63,6 @@ DoublyLinkedList<string> User::getUserInterests() {
     return interests;
 }
 
-DoublyLinkedList<int> User::getUserConnections() {
-    return connections;
-}
 
 // Additional methods
 
