5bbb890114f5f2d6c3e1363b9ada17e0c34b09df
taciturn2021 <darkraiden112@gmail.com>
2024-12-12 15:00:28 +0500

    Add ConnectionsManager, Add iterator support

--- ConnectionsManager.h
+++ ConnectionsManager.h
@@ -4,11 +4,22 @@
 
 #ifndef CONNECTIONSMANAGER_H
 #define CONNECTIONSMANAGER_H
-
-
-
+using namespace std;
+#include <vector>
+#include "DoublyLinkedList.h"
 class ConnectionsManager {
-
+private:
+    DoublyLinkedList<pair<int, int>> connections;
+public:
+    ConnectionsManager();
+    void addConnection(int userID1, int userID2);
+    bool areConnected(int userID1, int userID2);
+    void removeConnection(int userID1, int userID2);
+    vector<int> getConnectionsByUser(int userId);
+    void displayConnections();
+    void displayMutualConnections();
+    void displayUserConnections();
+    void displayUserConnections(int userId);
 };
 
 
