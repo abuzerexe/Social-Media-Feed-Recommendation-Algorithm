3e10f1e1b3f5b84c5df19ba53bf279c005130e9a
a-muizz28 <abdul.muizz2005@gmail.com>
2024-12-12 22:40:05 +0500

    Created the User Analytics Class and header file in the project.

---
+++ UserAnalytics.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Abdul Muizz on 12/12/2024.
+//
+
+#include "UserAnalytics.h"
+#include <vector>
+#include <iostream>
+using namespace std ;
+
+std::pmr::vector<User> UserAnalytics::getMostActiveUsers() {
+
+}
+
---
+++ UserAnalytics.h
@@ -0,0 +1,24 @@
+//
+// Created by Abdul Muizz on 12/12/2024.
+//
+
+#ifndef USERANALYTICS_H
+#define USERANALYTICS_H
+#include <vector>
+#include "User.h"
+using namespace std ;
+
+
+class UserAnalytics {
+
+public:
+
+    std::pmr::vector<User> getMostActiveUsers();
+    std::pmr::vector<User> getInfluentialUsers();
+    int getIsolatedUserCount() ;
+
+};
+
+
+
+#endif //USERANALYTICS_H
